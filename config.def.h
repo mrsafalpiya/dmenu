7a4e19dfd4746811f2802cbdee4eb570e540ee28
Safal Piya <mrsafalpiya@gmail.com>
2021-08-23 19:00:51 +0545

    Changed fonts to use Noto Color Emoji only for emoji

--- config.def.h
+++ config.def.h
@@ -6,8 +6,7 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Source Code Pro:pixelsize=14",
-	"JoyPixels:pixelsize=14",
-	"Symbols Nerd Font:pixelsize=14"
+	"Noto Color Emoji:pixelsize=14",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {

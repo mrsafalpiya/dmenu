71c4f43e1fb998b95794155bf4515aaacf1a37aa
Safal Piya <mrsafalpiya@gmail.com>
2021-09-05 21:16:09 +0545

    Changed to Terminus (TTF) font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Source Code Pro:pixelsize=14",
+	"Terminus (TTF):pixelsize=22",
 	"Noto Color Emoji:pixelsize=14",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
